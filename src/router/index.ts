e9c0f97744fb29bd858341143f542b027558db9b
fouz0062 <fouzifathii@gmail.com>
2025-11-08 21:11:31 +0100

    deleted node modules and package lock file and re installed using npm i

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,31 +1,33 @@
-import { h } from 'vue'
+// import { h } from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
-
+import { routes } from 'vue-router/auto'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('@/views/HomeView.vue')
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue')
-    },
-    {
-      // /projects/aghshjfdeghwfui
-      path: '/projects/:id',
-      name: 'single-projects',
-      component: () => import('@/views/SingleProjectView.vue')
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'Not Found',
-      component: h('p', { style: 'color: red;fontSize: 8rem;' }, '404 Not Found')
-    }
-  ]
+  routes
 })
 
 export default router
+
+// routes: [
+//   {
+//     path: '/',
+//     name: 'home',
+//     component: () => import('@/views/HomeView.vue')
+//   },
+//   {
+//     path: '/projects',
+//     name: 'projects',
+//     component: () => import('@/views/ProjectsView.vue')
+//   },
+//   {
+//     // /projects/aghshjfdeghwfui
+//     path: '/projects/:id',
+//     name: 'single-projects',
+//     component: () => import('@/views/SingleProjectView.vue')
+//   },
+//   {
+//     path: '/:catchAll(.*)*',
+//     name: 'Not Found',
+//     component: h('p', { style: 'color: red;fontSize: 8rem;' }, '404 Not Found')
+//   }
+// ]
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    // 1. VueRouter MUST be placed before the main Vue plugin
+    VueRouter({
+      /* You can add options here if needed, like your pages directory */
+    }),
+    // 2. Main Vue plugin
     vue(),
-    vueDevTools(),
+    // 3. DevTools (can be placed anywhere after core plugins)
+    vueDevTools()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
