a3e343de8fe08023db7c56e75b9a2e3c744cea6b
fouz0062 <fouzifathii@gmail.com>
2025-11-08 20:29:41 +0100

    created two components under view and updated routing

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import Home from '@/views/HomeView.vue'
+import Projects from '@/views/ProjectsView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: Projects
+    }
+  ]
 })
 
 export default router
---
+++ src/views/ProjectsView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    <h1>Projects Page</h1>
+    <a href="/"></a>
+  </div>
+</template>
+
+<style scoped></style>
